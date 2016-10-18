5e8d4dcdbe7ef8c658b2b0ea725c511f0509540e
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-18 12:38:59 +0200

    Bump to 0.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mys:typescript-ng-annotate',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'TypeScript for Meteor with ngAnnotate',
   git: 'https://github.com/kamilkisiela/typescript-ng-annotate',
   documentation: 'README.md'
@@ -16,12 +16,12 @@ Package.registerBuildPlugin({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.0.1');
+  api.versionsFrom('1.3.1');
 
   api.use('isobuild:compiler-plugin@1.0.0');
-  api.use('barbatus:typescript-compiler@0.5.7');
+  api.use('barbatus:typescript-compiler@0.7.0');
 
-  api.imply('modules@0.5.2');
+  api.imply('modules@0.6.4');
 
-  api.imply('barbatus:typescript-runtime@0.1.1');
+  api.imply('barbatus:typescript-runtime@0.1.2');
 });
